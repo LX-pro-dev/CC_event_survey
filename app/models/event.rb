7ad3a21fb2ed07dd08f7018d1582b3b88bedf9cb
LX-pro-dev <lx.pro.dev@gmail.com>
2021-03-19 10:37:26 +0100

    UX + pages' francization

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   has_many :surveys, dependent: :destroy
   
   validates :status, presence: true
-  STATUSES = [:Indéfini, :Prévu, :Terminé]
+  STATUSES = [:indéfini, :prévu, :terminé]
 
   def total_event
     surveys.sum(:total)
